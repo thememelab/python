754b15da4372a04b70802fe8700bbe05637938dc
thememelab <thememelab@icloud.com>
2022-06-09 11:48:56 +0200

    sets added

---
+++ sets.py
@@ -0,0 +1,66 @@
+#Create a Set:
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+#Set items can be of any data type:
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+set4 = {"abc", 34, True, 40, "male"}
+
+#Loop through the set, and print the values:
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+print(x)
+
+#Check if "banana" is present in the set:
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+#Add an item to a set, using the add() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+#Add elements from tropical into thisset:
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+print(thisset)
+
+
+#thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+
+#Remove "banana" by using the remove() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+#thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+#Loop through the set, and print the values:
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+ Print(x)
+
+
+#he union() method returns a new set with all items from both sets:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+#The update() method inserts the items in set2 into set1:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+
+#set methids
+#https://www.w3schools.com/python/python_sets_methods.asp
